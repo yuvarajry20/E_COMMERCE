7e549a1b8622fa9286aeeae83f3fb76a1576ea7e
jeevika <kirubajeevika@gmail.com>
2025-02-13 20:09:39 +0530

    created user class in model package

---
+++ src/com/ecommerce/model/User.java
@@ -0,0 +1,65 @@
+	package com.ecommerce.model;
+
+	public class User {
+	    private int userId;
+	    private String username;
+	    private String password;
+	    private String role;
+
+	    // Default Constructor
+	    public User() {
+	    }
+
+	    // Parameterized Constructor
+	    public User(int userId, String username, String password, String role) {
+	        this.userId = userId;
+	        this.username = username;
+	        this.password = password;
+	        this.role = role;
+	    }
+
+	    // Getters and Setters
+	    public int getUserId() {
+	        return userId;
+	    }
+
+	    public void setUserId(int userId) {
+	        this.userId = userId;
+	    }
+
+	    public String getUsername() {
+	        return username;
+	    }
+
+	    public void setUsername(String username) {
+	        this.username = username;
+	    }
+
+	    public String getPassword() {
+	        return password;
+	    }
+
+	    public void setPassword(String password) {
+	        this.password = password;
+	    }
+
+	    public String getRole() {
+	        return role;
+	    }
+
+	    public void setRole(String role) {
+	        this.role = role;
+	    }
+
+	    // To String Method (For Debugging)
+	    @Override
+	    public String toString() {
+	        return "User{" +
+	                "userId=" + userId +
+	                ", username='" + username + '\'' +
+	                ", password='" + password + '\'' +
+	                ", role='" + role + '\'' +
+	                '}';
+	    }
+	}
+
